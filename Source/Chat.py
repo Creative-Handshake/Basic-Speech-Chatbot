a8352692a07d7f53299e2c254a959e5e3fafdf28
Ethan Barr <70385874+Ethan-Barr@users.noreply.github.com>
2021-08-13 18:22:52 +0100

    Update Chat.py

--- Source/Chat.py
+++ Source/Chat.py
@@ -4,11 +4,77 @@ import pyaudio
 import pyttsx3
 
 import datetime
-import 
+import time
 
 
 
 #Variables
+user = '' #Enter your name in the ''
+
+hour = int(datetime.datetime.now().hour)
+strTime = datetime.datetime.now().strftime("%H:%M")
+date = datetime.datetime.now().strftime("%A:%B:%Y")
+
+r = sr.Recognizer()
+
 
 
 #Speech-recognition setup
+engine = pyttsx3.init()
+voices = engine.getProperty('voices')
+engine.setProperty('voice', voices[0].id)
+engine.setProperty('rate', 150)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+    
+    
+#Taking the command
+def command():
+	with sr.Microphone() as source:
+        r.pause_threshold = 1
+        audio = r.listen(source)
+        
+        try:
+            query = r.recognize_google(audio, language='en-gb')
+            print(f"Ethan: {query}\n")
+        
+        except Exception as e:
+            print("Exception: " + str(e))
+        
+    return query
+
+
+
+#Tells you the time of day (Morning, afternoon, evening, night) - when you open the script
+def Welcome():
+	f hour>=0 and hour<12:
+        speak('Good morning ' + user)
+    
+    elif hour>=12 and hour<18:
+        speak('Good afternoon ' + user)
+
+    elif hour >=18 and hour>0:
+        speak('Good evening ' + user)
+
+    else:
+        speak("Good night " + user)
+		
+
+		
+#Main Script
+print("[BOT] : Running")
+print(" ")
+
+time.sleep(2)
+Welcome()
+
+while True:
+	query = Command()
+	
+	if query == "Hello":
+		speak("Hello" + user)
+		
+	
